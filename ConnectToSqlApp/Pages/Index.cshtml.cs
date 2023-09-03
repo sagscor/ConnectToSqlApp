9644a7d381b0482ad0ff6a793a5928cffd94f22e
LENOVO <LENOVO@LAPTOP-UNCRUK2B>
2023-09-04 00:13:41 +0530

    Add project files.

---
+++ ConnectToSqlApp/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using ConnectToSqlApp.Models;
+using ConnectToSqlApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConnectToSqlApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<Product> Products;
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+        }
+    }
+}
